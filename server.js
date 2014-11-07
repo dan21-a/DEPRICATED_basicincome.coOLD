0e59c4942f326f27681e431b2dca8d0114d01d5a
resilience_me <emailsareobsolete@gmail.com>
2014-11-07 15:19:52 +0000

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 //--- server.resilience.me --------------
 
+// I´ve improvised ALOT of solutions. Think of this as a SKETCH.
+
 //----- Open a websocket for http://client.resilience.me -------
 
 var ws = require("nodejs-websocket");
@@ -80,7 +82,7 @@ var transaction_log_schema = new Schema({
 }, { collection: "transaction_log" });
 
 
-var account_data = mongoose.model('account_data', dividend_pathways_schema, ACCOUNT_ID);
+var account_data = mongoose.model('account_data', dividend_pathways_schema, COLLECTION);
 
 var transaction_log = mongoose.model('transaction_log', transaction_log_schema, "transaction_log");
 
@@ -116,49 +118,46 @@ var doc_obj3;
 
 if(TAX_BLOB[0].transaction_id !== undefined) {
 
-    update_dividend_pathway();
+    update_dividend_pathway()
 
 }
 
 function loop() {
-        i++;
+            i++;
+
     if(i<TAX_BLOB.length) {
         update_dividend_pathway();
 
     }
-    else SENT_BLOB_loop();
+    else update_transaction_log_with_TAX_BLOB();
 
 }
+
+//--------- first half of loop() --------------------------
 
 function update_dividend_pathway(){
 
     console.log("Scanning TAX_BLOB"+"["+[i]+"]"+" : " + JSON.stringify(TAX_BLOB[i]));
     
-  temp.account = TAX_BLOB[i].account;   
-  temp.currency = TAX_BLOB[i].currency;
-  temp.taxRate = TAX_BLOB[i].taxRate;
-  temp.amount = TAX_BLOB[i].amount;
 
   // the code below updates db.ACCOUNT_ID.account_data:
 
   var query = account_data.find( {account: TAX_BLOB[i].account, currency: TAX_BLOB[i].currency, taxRate: (String(TAX_BLOB[i].taxRate))}
   , function(err, doc){
-      doc_obj = doc;
     query.count(function(err, count){
-      doc_count = count;
-something();
-    });
-    
-    function something(){
+        doc_count = count
+        something()
+    });//end of query.count
+        function something(){
         if (doc_count > 0) {   
-            console.log(doc_count + " documents found:");
-            //console.log(doc_obj);
+            console.log(doc_count + " documents found");
+            //console.log(doc);
             console.log("updating...");
-        var id = doc_obj[0]._id;
+        var id = doc[0]._id;
         
         account_data.findById(id, function (err, data) {
 
-                var total_pathway = Number(data.total_pathway) + Number(temp.amount);
+                var total_pathway = Number(data.total_pathway) + Number(TAX_BLOB[i].amount);
                 
                 data.total_pathway = String(total_pathway);
                 data.save(function (err) {
@@ -169,17 +168,17 @@ something();
         }
         
         else { console.log(doc_count + " document found, adding new document...");
-          var add_new = new account_data({ account: temp.account, connected_transactions: {transaction_id: String}});
- add_new.save(function (err) {
-  if (err) // ...
-  console.log('saved');
-});
+          var add_new = new account_data({ account: TAX_BLOB[i].account, connected_transactions: {transaction_id: String}});
+        add_new.save(function (err) {
+        if (err) // ...
+        console.log('saved');
+        });
 
- console.log("New entry created"); 
-update_taxRate_upper_limit();
+        console.log("New entry created"); 
+        update_taxRate_upper_limit();
+        }
+    
 }
-    }
-
   }//end of query.exec
       
       
@@ -196,11 +195,11 @@ function update_taxRate_upper_limit(){
 
   // the code below updates db.TAX_BLOB[i].taxRate_upper_limit:
 
-  var query2 = taxRate_upper_limit.find( {ccurrency: TAX_BLOB[i].currency, ttaxRate: TAX_BLOB[i].taxRate}
+  var query = taxRate_upper_limit.find( {ccurrency: TAX_BLOB[i].currency, ttaxRate: TAX_BLOB[i].taxRate}
   , function(err, doc){
       doc_obj3 = doc;
       //console.log(doc + doc._id);
-    query2.count(function(err, count){
+    query.count(function(err, count){
       doc_count2 = count;
 something();
     });
@@ -219,9 +218,10 @@ something();
                 data.ttotal_amount = String(ttotal_amount);
                 data.save(function (err) {
                 });
+                        loop();
+
         });
         
-        update_transaction_log_with_TAX_BLOB();
         }
         
         else { console.log(doc_count2 + " taxRate_upper_limit document found, adding new document...");
@@ -232,7 +232,7 @@ something();
 });
 
  console.log("New taxRate_upper_limit entry created"); 
-update_transaction_log_with_TAX_BLOB();
+loop();
 }
     }
 
@@ -246,12 +246,24 @@ update_transaction_log_with_TAX_BLOB();
 
 
 
+//--------- second half of loop() --------------------------
+
+
+function TAX_BLOB_loop() {
+                k++;
+
+    if(k<TAX_BLOB.length) {
+        update_transaction_log_with_TAX_BLOB();
+    }
+    else k = 0, update_transaction_log_with_SENT_BLOB();
+
+}
 
 
     function update_transaction_log_with_TAX_BLOB() {
         
         var doc_obj;
-        var query = transaction_log.find( {account: TAX_BLOB[i].account}
+        var query = transaction_log.find( {account: TAX_BLOB[k].account}
         , function(err, doc){
             
             doc_obj = doc;
@@ -262,23 +274,24 @@ update_transaction_log_with_TAX_BLOB();
     
             function something(){
                 if (doc_count > 0) { 
-                     console.log(doc_count + " transaction_logs found");
+                     console.log(doc_count + " TAX_BLOB transaction logs found");
 
                     var id = doc_obj[0]._id;
                     //console.log(id);
-                    transaction_log.update({ _id: id }, { $push:{connected_transactions: {transaction_id: TAX_BLOB[i].transaction_id}}}, function(rawResponse){console.log(rawResponse)}); 
-            
+                    transaction_log.update({ _id: id }, { $push:{connected_transactions: {transaction_id: TAX_BLOB[k].transaction_id}}}); 
+                  //transaction_log.update({ _id: id }, { $push:{connected_transactions: {transaction_id: TAX_BLOB[k].transaction_id}}}, function(rawResponse){console.log(rawResponse)}); 
+
     
   
-                                       loop();   
+                                       TAX_BLOB_loop();   
 
 
                 }
                 
 
-                else { console.log(doc_count + " transaction_logs found, adding new document...");
+                else { console.log(doc_count + " TAX_BLOB transaction logs found, adding new document...");
                           
-                    var add_new = new transaction_log({ account: temp.account, connected_transactions: [{transaction_id: TAX_BLOB[i].transaction_id}]});
+                    var add_new = new transaction_log({ account: TAX_BLOB[k].account, connected_transactions: [{transaction_id: TAX_BLOB[k].transaction_id}]});
                     add_new.save(function (err) {
                     if (err) // ...
                     console.log('saved');
@@ -286,7 +299,7 @@ update_transaction_log_with_TAX_BLOB();
                 
                     console.log("transaction logged"); 
                     
-                    loop(); 
+                    TAX_BLOB_loop(); 
                 }
             }    
 
@@ -295,9 +308,9 @@ update_transaction_log_with_TAX_BLOB();
 
 function SENT_BLOB_loop() {
         k++;
+
     if(k<SENT_BLOB.length) {
         update_transaction_log_with_SENT_BLOB();
-
     }
     else pingback();
 
@@ -313,42 +326,41 @@ function SENT_BLOB_loop() {
             doc_obj = doc;
             query.count(function(err, count){
                 doc_count = count;
-                something();
-            });
-    
-            function something(){
+                
+                //console.log("scanning SENT_BLOB"+"["+k+"]")
+
                 if (doc_count > 0) { 
-                     console.log(doc_count + " transaction_logs found");
+                     //console.log(doc_count + " SENT_BLOB transaction logs found");
 
                     var id = doc_obj[0]._id;
-                    console.log(id);
-                    transaction_log.update({ _id: id }, { $push:{connected_transactions: {transaction_id: SENT_BLOB[k].transaction_id}}}, function(rawResponse){console.log(rawResponse)}); 
+                    //console.log(id);
+                    transaction_log.update({ _id: id }, { $push:{connected_transactions: {transaction_id: SENT_BLOB[k].transaction_id}}}); 
             
     
-  
-                                          
+                     //SENT_BLOB_loop()
+                              pingback()            
 
 
                 }
                 
 
-                else { console.log(doc_count + " transaction_logs found, adding new document...");
+                else { console.log(doc_count + " SENT_BLOB transaction_logs found, adding new document...");
                           
-                    var add_new = new transaction_log({ account: temp.account, connected_transactions: [{transaction_id: SENT_BLOB[k].transaction_id}]});
+                    var add_new = new transaction_log({ account: TAX_BLOB[k].account, connected_transactions: [{transaction_id: SENT_BLOB[k].transaction_id}]});
                     add_new.save(function (err) {
                     if (err) // ...
                     console.log('saved');
                     });
                 
                     console.log("transaction logged"); 
-                    
+                    //SENT_BLOB_loop()
+                    pingback()
                 }
-            }    
+               
+            });//end query.count
+        });//end query.exec
 
-        });
-            total_amount();
-
-    }
+    }//end update_transaction_log_with_SENT_BLOB()
 
 
 
@@ -366,7 +378,10 @@ function SENT_BLOB_loop() {
                         
                        
         conn.sendText(JSON.stringify(PING_BLOB));
+                    
         
+        total_amount();
+
     }
 
 
@@ -382,8 +397,8 @@ function total_amount(){
     for (var f = 0; f < TAX_BLOB.length; f++) { 
         if(temp.indexOf(TAX_BLOB[f].currency) == -1){
         IOUs.push({currency: TAX_BLOB[f].currency, total_amount: ""});
-        temp+= TAX_BLOB[f].currency + " "
-        console.log(IOUs);
+        temp+= TAX_BLOB[f].currency + " ";
+        //console.log(IOUs);
         }
     }
     var j = 0;
@@ -414,13 +429,6 @@ swarm_redistribution();
 
 // script should then run swarm-redistribution algorithm
 
-
-// to Darklight: Here, let´s pretty much cut & paste the script @ http://www.resilience.me/swarm_redistributionjs.html
-
-// find dividend_pathway-lines (example: http://www.resilience.me/swarm_redistributionjs.html)
-
-
-
 function swarm_redistribution(){
 
     var lines = [];//lines.push(line)
@@ -430,35 +438,37 @@ function swarm_redistribution(){
    
     var temp = " ";
     
-    COLLECTION = ACCOUNT_ID;
-    
     dividend_lines();
     
+    
+// ------- FIRST, dividend_lines() -----------------
+
     function dividend_lines() {
         
+       
         console.log("scanning collection: "+ COLLECTION);
     
         var q = 0; //IOUs[q]
-    
-    
-// STEP 1: list all dividend pathways in IOUs[0] for ACCOUNT_ID
+        
+        account_data = mongoose.model('account_data', dividend_pathways_schema, COLLECTION);//to insert new COLLECTION
 
-    var query = account_data.find({currency: IOUs[q].currency}, function(err, doc) {
+
+// STEP 1: list all dividend pathways in IOUs[0] for ACCOUNT_ID
+    account_data.find({currency: IOUs[q].currency} ).exec(function(err, doc) {
+    
+    if(doc.length > 0){
+
         //console.log(doc);
         var w = 0; //doc[w]
         var line = [];//[{account: doc[w].account}]
 
         loop(doc);
         
-        function loop(doc) {
-            /*
-            console.log(doc);
-            console.log(doc[w].account);
-            console.log[lines]
-            
-            console.log("hej" + temp)
-            console.log(temp.indexOf(doc[w].account));
-            */
+    }
+    else console.log("collection is empty"), recursion()
+
+function loop(doc) {
+
             var q = 0
             
             if (temp.indexOf(doc[w].account) == -1){
@@ -467,14 +477,15 @@ function swarm_redistribution(){
             line.push({account: doc[w].account});
             q++
             }
-            else line.push("[CIRCULAR]");
+            else console.log("CIRCULAR");
             
             w++;
             
             if (w<doc.length){loop(doc)}
             else {
-                console.log(line);//lists all dividend pathways in IOUs[0] for ACCOUNT_ID
                 if (q>0){
+                    console.log(line);//lists all dividend pathways in IOUs[0] for ACCOUNT_ID
+
                     lines.push(line)
                 };
                 
@@ -482,15 +493,6 @@ function swarm_redistribution(){
 
             }
         }
-        
-  
-        /*
-        console.log("hej" + line)
-
-        console.log(lines);
-        */
-        
-  
 
 
     });//end of function(err, doc)
@@ -500,41 +502,46 @@ function swarm_redistribution(){
     
       
 // STEP 2: recursion (add all dividend pathways for lines[x][i].account)        
-// to Darklight: not sure how to do this:
-   
+
 
     function recursion(){
             if(x<lines.length){
                 
         if(y<lines[x].length){
             COLLECTION = lines[x][y].account;
-            console.log(COLLECTION)
             y++;
             dividend_lines();
             
         }
                 
         else {
-           
+           x++;
+           y = 0;
             console.log("recursion nr "+x)
             
-            x++;
-            y = 0;
+            
             dividend_lines()
         }
         
             }
-            else console.log("END")
+            else console.log(lines), console.log("END")
         
-        /*    
-    if(){           
-    }
-    else console.log("END");
-    */
+
 }
+         
+         
+         
+         
+// ------- SECOND, taxRate-ratios -----------------
+// see http://www.resilience.me/theory.html
+         
          
         
 }//end swarm_redistribution()
+
+
+
+
 
 
 
